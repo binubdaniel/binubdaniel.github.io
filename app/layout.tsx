30bde039b1eff58bd6e10af8c8c9872b6f77fa91
scfeadmin <superadmin@socife.com>
2025-03-06 23:19:13 +0530

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Analytics />
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
